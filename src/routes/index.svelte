4c5646d4cc7a6c461c796402dc4b3712f870dd64
Sean Pettit <s.p@mac.com>
2022-04-30 18:15:26 +0100

    add bar chart

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,12 +1,16 @@
 <script>
 	import { DatePicker, DateRangePicker } from 'toastcomponents';
 	import ToastEditor from 'toasteditor';
+	import { BarChart } from 'toastcharts';
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Welcome to the Toastie Demo Site</h1>
 
 <br />
 <DatePicker />
+<br />
 <DateRangePicker />
+<br />
 <ToastEditor code={true} />
+<br />
+<BarChart />
