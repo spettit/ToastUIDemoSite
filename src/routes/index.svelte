369c90ff38dea1788b30322a45e344a2e1e5daca
Sean Pettit <s.p@mac.com>
2022-04-30 17:52:16 +0100

    first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,12 @@
+<script>
+	import { DatePicker, DateRangePicker } from 'toastcomponents';
+	import ToastEditor from 'toasteditor';
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<br />
+<DatePicker />
+<DateRangePicker />
+<ToastEditor code={true} />
